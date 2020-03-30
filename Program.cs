2ae75c305252c1573373af5bca254cfbfbb50fa8
Yurii Moroz <yurii.moroz.pm.2018@lpnu.ua>
2020-03-30 19:33:02 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DZ_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:/Users/Admin/RiderProjects/Program_Todo/NewFile1.txt";
+            string tasck;
+            tasck = Console.ReadLine();
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                sw.WriteLine(tasck);
+            }
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                int i = 1;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(i+" "+line);
+                    i++;
+                }
+            }
+        }
+    }
+}
