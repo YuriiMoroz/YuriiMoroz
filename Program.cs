0eedae36f15fd750b76f5d464fe7f4ce643c3ae5
Yurii Moroz <yurii.moroz.pm.2018@lpnu.ua>
2020-04-01 14:50:11 +0300

    Program wich can add and delete tascks

--- Program.cs
+++ Program.cs
@@ -12,32 +12,76 @@ namespace DZ_2
             string path = "C:/Users/Admin/RiderProjects/DZ_2/NewFile1.txt";
             string tasck;
             List<string> list=new List<string>();
-            tasck = Console.ReadLine();
-            using (StreamWriter sw = new StreamWriter(path, true))
-            {
-                sw.WriteLine(tasck);
-                sw.Close();
-            }
+            bool tumbler = true;
             using (StreamReader sr = new StreamReader(path))
             {
                 string line;
+                int k = 1;
                 while ((line = sr.ReadLine()) != null)
                 {
-                  list.Add(line);
+                    list.Add(line);
+                    Console.WriteLine(k+" "+line);
+                    k++;
                 }
             }
-            File.WriteAllText(path, string.Empty);
-            int i = 1;
-            for (int j = 0; j <list.Count; j++)
+            while (tumbler)
             {
-                if (j == 3 - 1)
-                    continue;
-                Console.WriteLine(i+" "+list[j]);
-                i++;
-                using (StreamWriter sw1 = new StreamWriter(path, true))
+                Console.WriteLine(
+                    "If you want to add new tasck print 'add' " +
+                    "\nIf you wont to delete your taskk print 'delete'" +
+                    "\nIf you want to exit print 'exit' ");
+                string add = Console.ReadLine();
+                if (add == "exit")
+                    tumbler = false;
+                if (add == "add")
                 {
-                    sw1.WriteLine(list[j]);
-                    sw1.Close();
+                    Console.WriteLine("input your tasck");
+                    tasck = Console.ReadLine();
+                    using (StreamWriter sw = new StreamWriter(path, true))
+                    {
+                        sw.WriteLine(tasck);
+                    }
+
+                    /*using (StreamReader sr = new StreamReader(path))
+                    {
+                        string line;
+                        int k = 1;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            Console.WriteLine(k + " " + line);
+                            k++;
+                        }
+                    }*/
+                    using (StreamReader sr = new StreamReader(path))
+                    {
+                        string line;
+                        list.Add("tasck");
+                        int k = 1;
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            list[k - 1] = line;
+                            Console.WriteLine(k + " " + line);
+                            k++;
+                        }
+                    }
+                }
+
+                if (add == "delete")
+                {
+                    Console.WriteLine("Print the number of tasck");
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    File.WriteAllText(path, string.Empty);
+                    list.Remove(list[number - 1]);
+                    File.AppendAllText(path, "");
+                    for (int j = 0; j < list.Count; j++)
+                    {
+
+                        using (StreamWriter sw = new StreamWriter(path, true))
+                        {
+                            sw.WriteLine(list[j]);
+                            Console.WriteLine(j+1+" "+list[j]);
+                        }
+                    }
                 }
             }
         }
